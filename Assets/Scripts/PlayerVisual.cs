65635f407f861c9f6f4d2072e663cc1bae2a665b
AdrichanFE <adrielpelao_xty@hotmail.com>
2024-12-11 21:22:25 +0100

    CosasDeNPCyAnimaciones

---
+++ Assets/Scripts/PlayerVisual.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerVisual : MonoBehaviour
+{
+
+    private Animator anim;
+    private float velocidad;
+    [SerializeField] private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocidad = agent.velocity.magnitude / agent.speed;
+        //Durante todos los frames voy actualizando la velocidad actual
+        anim.SetFloat("Velocity", velocidad);
+    }
+}
